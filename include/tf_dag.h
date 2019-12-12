c39b46b3a886ab55e9c1f921e2a3341c6b1592e6
Tom Stewart <thomas@ascent.ai>
2019-12-12 17:55:29 +0900

    works with /tf

--- include/tf_dag.h
+++ include/tf_dag.h
@@ -3,6 +3,7 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 class TFDAG
 {
@@ -15,3 +16,11 @@ public:
     explicit TFDAG(ros::NodeHandle &nh);
     void frame_cb(const tf2_msgs::TFMessage::ConstPtr &msg);
 };
+
+
+inline tf2::Stamped<tf2::Transform> from_msg(const geometry_msgs::TransformStamped &in)
+{
+    tf2::Stamped<tf2::Transform> out;
+    tf2::convert(in, out);
+    return out;
+}
--- src/tf_dag.cpp
+++ src/tf_dag.cpp
@@ -1,9 +1,9 @@
 #include "tf_dag.h"
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 using namespace tf2_msgs;
 
-TFDAG::TFDAG(ros::NodeHandle &nh) : tf_listener(tf_buffer)
+
+TFDAG::TFDAG(ros::NodeHandle &nh) : tf_listener(tf_buffer), tf_buffer(ros::Duration(1))
 {
     frame_sub = nh.subscribe<tf2_msgs::TFMessage>("tf_dag", 1, &TFDAG::frame_cb, this);
 }
@@ -14,51 +14,25 @@ void TFDAG::frame_cb(const TFMessage::ConstPtr &msg)
 
     for(auto transform : msg->transforms)
     {
-        // B wants to publish to C but that's no bueno because world is already publishing to C
-        // to get B into the graph
-
-        // so 
-
-        if(tf_buffer._frameExists(transform.child_frame_id) && tf_buffer._getParent(transform.child_frame_id, transform.header.stamp, parent) && parent != transform.header.frame_id)
+        if(tf_buffer._frameExists(transform.child_frame_id) &&
+            tf_buffer._getParent(transform.child_frame_id, ros::Time(0), parent) &&
+            parent != transform.header.frame_id)
         {
             child = transform.child_frame_id;
 
             // Scan up the tree until we reach a frame without a parent
-            while(tf_buffer._getParent(child, transform.header.stamp, parent)) 
+            while(tf_buffer._getParent(child, ros::Time(0), parent) && parent != transform.header.frame_id)
                 child = parent;
 
-            ROS_ERROR_STREAM(transform.header.frame_id << " " << transform.child_frame_id << " " << parent);
+            auto transform_to_ancestor = tf_buffer.lookupTransform(transform.child_frame_id, child, ros::Time(0), ros::Duration(0));
+
+            transform.child_frame_id = child;
+            transform.transform = tf2::toMsg(from_msg(transform_to_ancestor) * from_msg(transform));
         }
 
+        tf_buffer.setTransform(transform, "tf_dag");
         tf_broadcaster.sendTransform(transform);
     }
-
-
-    // std::string parent;
-
-    // for(auto transform : msg->transforms)
-    // {
-    //     // If the frame is in the tree, it has a parent and that parent doesn't match the transform's source frame
-    //     if(tf_buffer._frameExists(transform.child_frame_id) &&
-    //         tf_buffer._getParent(transform.child_frame_id, transform.header.stamp, parent) &&
-    //         parent != transform.header.frame_id) 
-    //     {
-    //         tf2::Stamped<tf2::Transform> transform_to_ancestor, transform_to_child;
-
-    //         // Scan up the tree until we reach a frame without a parent
-    //         while(tf_buffer._getParent(transform.child_frame_id, transform.header.stamp, parent)) ;
-
-    //         // publish the inverse of the transform between the target frame and the parent
-    //         tf2::convert(tf_buffer.lookupTransform(transform.header.frame_id, parent, transform.header.stamp, ros::Duration(0)), transform_to_ancestor);
-    //         tf2::convert(transform, transform_to_child);
-
-    //         transform.transform = tf2::toMsg((transform_to_ancestor * transform_to_child).inverse());
-    //         transform.child_frame_id = parent;
-    //         transform.header.frame_id = transform.header.frame_id;
-    //     }
-
-    //     tf_broadcaster.sendTransform(transform);
-    // }
 }
 
 int main(int argc, char** argv)
