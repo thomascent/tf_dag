0dc2517890546410ccecb8d310c028b5ab8398a4
Tom Stewart <thomas@ascent.ai>
2019-12-13 11:39:39 +0900

    tests actually work now

--- src/test/tf_test.cpp
+++ src/test/tf_test.cpp
@@ -1,13 +1,31 @@
-// Bring in my package's API, which is what I'm testing
-#include "foo/foo.h"
-
+#include <tf2_ros/transform_listener.h>
 #include <gtest/gtest.h>
 
-// Declare another test
-TEST(TestSuite, testCase2)
+
+TEST(TestSuite, test_tf)
 {
-    // <test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+    std::string parent_frame, child_frame;
+    std::vector<double> translation, rotation;
+
+    ros::param::get("~parent_frame", parent_frame);
+    ros::param::get("~child_frame", child_frame);
+    ros::param::get("~translation", translation);
+    ros::param::get("~rotation", rotation);
+
+    tf2_ros::Buffer buffer;
+    tf2_ros::TransformListener listener(buffer);
+
+    auto transform_stamped = buffer.lookupTransform(parent_frame, child_frame, ros::Time(0), ros::Duration(5));
+
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.translation.x, translation[0]);
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.translation.y, translation[1]);
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.translation.z, translation[2]);
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.rotation.x, rotation[0]);
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.rotation.y, rotation[1]);
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.rotation.z, rotation[2]);
+    EXPECT_DOUBLE_EQ(transform_stamped.transform.rotation.w, rotation[3]);
 }
+
 
 int main(int argc, char **argv)
 {
